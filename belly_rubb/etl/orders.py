3f01b06547e11d62eb2f492516369dc5dd8e53d0
Arsen Jamkotchian <arsenjamkotchian@gmail.com>
2025-08-16 11:28:33 -0700

    Resolve conflict in orders.py

--- belly_rubb/etl/orders.py
+++ belly_rubb/etl/orders.py
@@ -1,5 +1,4 @@
 from square import Square
-<<<<<<< HEAD
 from belly_rubb.etl.api_manager import APIManager
 from belly_rubb.etl.token_provider import TokenProvider
 
@@ -17,18 +16,4 @@ class OrdersAPI:
         self.api_manager = APIManager()
 
     def _paginated_orders(self, page_limit: int = 50):
-=======
-
-from app.db import Session
-from belly_rubb.etl.token_provider import TokenProvider
-
-class OrdersAPI:
-    def __init__(self, merchant_id: str):
-        provider = TokenProvider()
-        token = provider.get_access_token(merchant_id=merchant_id)
-
-        self.client = Square(token=token)
-
-    def _get_order_ids(self):
->>>>>>> 8fba5b64c3b48975e03f8a792eb695cf9bc01310
         pass
